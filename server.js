abe68b78a5e4849571215408b85a50014a698c40
udio <udio@iwobi.co.il>
2018-12-02 13:27:39 +0200

    projects page added

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ app.get('/', (req,res) => {
     });
 });
 
+app.get('/projects', (req,res) =>{
+    res.render('projects.hbs',{
+        pageTitle: 'Projects Page',
+    });
+});
+
+
 app.get('/about', (req,res) =>{
     res.render('about.hbs',{
         pageTitle: 'About Page',
